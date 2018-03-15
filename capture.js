c00615a6799764a5b16e2408b035935ddd50794f
Linux User <qingyanjiu@hotmail.com>
2018-03-15 08:15:50 +0000

    update

--- capture.js
+++ capture.js
@@ -7,19 +7,16 @@ page.viewportSize = { width: 1600, height: 900 };
 var url = args[1];
 var componentId = args[2];
 
-console.log(url);
-
 page.open(url, function() {
-
-    page.clipRect = page.evaluate(function() {
-        var obj = document.getElementsByClassName('well')[0].getBoundingClientRect();
-        if(componentId)
-            obj = document.getElementById(componentId).getBoundingClientRect();
+    page.clipRect = page.evaluate(function(id){
+        var obj =
+document.getElementById(id).getBoundingClientRect();
         return obj;
-    });
-  
+    },componentId);
+
   window.setTimeout(function () {
-        page.render('/home/revenueAssuranceMetrics.png',{quality: '100'});
+        page.render('/home/revenueAssuranceMetrics.png',{quality:
+'100'});
         phantom.exit();
     }, 10000);
 });
